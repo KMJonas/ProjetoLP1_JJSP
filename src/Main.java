317781eb97dac4aa72362f898630e64936292100
SergioP9340 <1231558@isep.ipp.pt>
2025-01-06 00:44:11 +0000

    merge

--- src/Main.java
+++ src/Main.java
@@ -11,17 +11,12 @@ import java.io.IOException;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) throws IOException {
-        /*
         MesaController mesaController = new MesaController();
         PratoController pratoController = new PratoController();
         ClienteReservaController crc = new ClienteReservaController();
 
         Menu menu = new Menu();
         menu.menu();
-         */
 
-        FicheirosLogController ficheirosLogController = new FicheirosLogController();
-
-        FicheirosLogController.criaFicheirosLog();
     }
 }
